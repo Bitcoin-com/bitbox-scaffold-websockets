c730533b32d40c43a592455a787601a260ac114c
Paul Bergamo <porlybe@gmail.com>
2018-07-29 11:51:14 +0900

    add pics and addresses

--- src/performers.js
+++ src/performers.js
@@ -1,32 +1,33 @@
-
-import placeholder from './assets/placeholder.png'
-
 import nana from './assets/nana.png'
 import kaoco from './assets/kaoco.jpg'
 import djsoulbird from './assets/djsoulbird.jpg'
-import yuka from './assets/yuka.jpg'
+import yuka from './assets/yuka.png'
+import ako from './assets/ako.png'
+import rie from './assets/rie.png'
+import yasmeen from './assets/yasmeen.png'
+import ayano from './assets/ayano.png'
 
 
 export const performers = {
-  "1QCNsitU6n5EhxF1abqarEvzbxDttNekHX": {
+  "19Z3RdS6R6AGa8TsYmhVnubpTP8C7cjDZY": {
     name: "Yasmeen",
-    image: placeholder
+    image: yasmeen
   },
-  "1Jk57sXmHmUZhYgssnb3oVTqgAkWWcw8hP": {
-    name: "Rie",
-    image: placeholder
-  },
-  "1CX8YjJjZtsKRwDakGiZAVWugNE4jGYg5s": {
+  "1AsuQDpZAHGRsUpa2CDUj7bo8PeXouBvdj": {
     name: "Ako",
-    image: placeholder
+    image: ako
+  },
+  "13UvstTMDgBJNGqyXetCZixendJ2KebeBQ": {
+    name: "Rie",
+    image: rie
   },
   "12TZWNBaCfHH4yL6SjQWvrrBaTgyuuj2xr": {
     name: "Yuka",
     image: yuka
   },
-  "1JxDnuXw6wdbtZf9cbrSdsz17ZqVxvZ5LZ": {
+  "14JgU2eMckpFxTjtS2gcH4GRJWmKHjegaG": {
     name: "Ayano",
-    image: placeholder
+    image: ayano
   },
   "17vhK8siMM15YPYdhDXqGmMJmMzVWcJxFa": {
     name: "Lady n@n@",
